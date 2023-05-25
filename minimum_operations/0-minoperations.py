4dbb5bf58df9cbe515133a0735b0bcc9ce4a2e1a
JoakBouy <B.joak@alustudent.com>
2023-05-25 15:50:57 +0300

    Minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def minOperations(n):
+    if n <= 1:
+        return n
+
+    dp = [0] * (n+1)
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = i     # Paste i-1 times
+        for j in range(1, i):
+            dp[i] = min(dp[i], dp[j] + (i-j))  # Copy All + Paste i-j times
+
+    if dp[n] == 0:
+        return 0
+    return dp[n]
